5be8df0f123936c365142182701cd75acac40616
reinaldoperes <48915319+reinaldoperes@users.noreply.github.com>
2020-07-07 16:34:58 -0300

    commit inicial

---
+++ src/pages/Main/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { FaGithub, FaPlus } from 'react-icons/fa'
+import {Container, Form, SubmitButton} from './styles';
+
+export default function Main(){
+    return(
+        <Container>
+            <h1>
+                <FaGithub size={25}/>
+                Meus repositórios
+            </h1>
+
+            <Form>
+                <input type="text" placeholder="Adicionar repositório"/>
+
+                <SubmitButton>
+                    <FaPlus color="#FFF" size={14} />
+                </SubmitButton>
+            </Form>
+        </Container>
+    )
+}
