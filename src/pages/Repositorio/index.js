9acd263e1647434954e41a046105e8181dd80c3b
reinaldoperes <48915319+reinaldoperes@users.noreply.github.com>
2020-07-09 17:48:35 -0300

    detalhamento do owner do repo e das issues, inicio da paginação

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,7 +1,79 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Container, Owner, Loading, BackButton, IssuesList, Pagination } from './styles';
+import { FaArrowLeft } from 'react-icons/fa';
+import api from '../../services/api';
 
-export default function Repositorio(){
+export default function Repositorio({match}){
+
+    const [repositorio, setRepositorio] = useState({});
+    const [issues, setIssues] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        
+        async function load(){
+            const nomeRepo = decodeURIComponent(match.params.repositorio);
+
+            const [repositorioData, issuesData] =  await Promise.all([
+                api.get(`/repos/${nomeRepo}`),
+                api.get(`/repos/${nomeRepo}/issues`, {
+                    params: {
+                        state: 'open',
+                        per_page: 5
+                    }
+                })
+            ]);
+
+            setRepositorio(repositorioData.data);
+            setIssues(issuesData.data);
+            setLoading(false);
+
+        }
+
+        load();
+
+    }, [match.params.repositorio]);
+
+    if (loading) {
+        return(
+            <Loading>
+                <h1>Carregando...</h1>
+            </Loading>
+        )
+    }
+
     return(
-        <h1>Repo</h1>
+        <Container>
+            <BackButton to="/">
+                <FaArrowLeft color="#000" size={30} />
+            </BackButton>
+            <Owner>
+                <img src={repositorio.owner.avatar_url} alt={repositorio.owner.login} />
+                <h1>{repositorio.name}</h1>
+                <p>{repositorio.description}</p>
+            </Owner>
+            <IssuesList>
+                {issues.map(issue => (
+                    <li key={issue.id.toString()}>
+                        <img src={issue.user.avatar_url} alt={issue.user.login} />
+                        <div>
+                            <strong>
+                                <a href={issue.html_url}>{issue.title}</a>
+                                {
+                                    issue.labels.map(label => (
+                                        <span key={label.id.toString()}>{label.name}</span>
+                                    ))
+                                }
+                            </strong>
+                            <p>{issue.user.login}</p>
+                        </div>
+                    </li>
+                ))}
+            </IssuesList>
+            <Pagination>
+                <button type="button" onClick={()=>{}}>Voltar</button>
+                <button type="button" onClick={()=>{}}>Próximo</button>
+            </Pagination>
+        </Container>
     )
 }
